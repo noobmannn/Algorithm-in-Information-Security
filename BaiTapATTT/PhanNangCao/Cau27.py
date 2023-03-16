c5505923c61547064f26e4a2975af7e5af94fdfb
Nguyễn Duy Bình <102444334+NguyenDuyBinh2003@users.noreply.github.com>
2023-03-16 19:49:32 +0700

    Create Cau27.py

---
+++ BaiTapATTT/PhanNangCao/Cau27.py
@@ -0,0 +1,34 @@
+import math
+
+
+def checkPrime(n):
+    if n <= 1:
+        return False
+    for i in range(2, (int(math.sqrt(n))) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def EuclideForFindGCD(a, b):
+    if a == 0:
+        return b
+    return EuclideForFindGCD(b % a, a)
+
+
+def findAB(a, b):
+    for i in range(a, b):
+        for j in range(i + 1, b):
+            if checkPrime(math.gcd(i, j)):
+                print(f"{i} {j}")
+
+
+def main():
+    a = int(input("Nhập A: "))
+    b = int(input("Nhập B: "))
+    print(f"Các cặp số thoả yêu cầu là: ")
+    findAB(a, b)
+
+
+if __name__ == "__main__":
+    main()
