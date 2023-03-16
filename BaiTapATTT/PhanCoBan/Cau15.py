cedaf62024bdd09b30c3efea41fb62613c9fb4b1
Nguyễn Duy Bình <102444334+NguyenDuyBinh2003@users.noreply.github.com>
2023-03-16 21:33:56 +0700

    Update Cau15.py

--- BaiTapATTT/PhanCoBan/Cau15.py
+++ BaiTapATTT/PhanCoBan/Cau15.py
@@ -1,6 +1,6 @@
 def sieveOfEratosthenes(n):
     listPrime = []
-    check = [True for i in range(n + 1)]
+    check = [True for _ in range(n + 1)]
     p = 2
     while p * p <= n:
         if check[p]:
