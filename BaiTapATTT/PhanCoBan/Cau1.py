d098e289d2cee025f877ee79a1ab862ede4146c6
Nguyễn Duy Bình <102444334+NguyenDuyBinh2003@users.noreply.github.com>
2023-04-12 15:53:38 +0700

    Create Cau1.py

---
+++ BaiTapATTT/PhanCoBan/Cau1.py
@@ -0,0 +1,24 @@
+def findQPrime(n):
+    d = 2
+    for i in range(2, int(n / 2 + 1)):
+        if n % i == 0:
+            d += 1
+    return d == 4
+
+
+def main():
+    n = int(input("Nhập vào N: "))
+    listRes = []
+    for i in range(1, n):
+        if findQPrime(i):
+            listRes.append(i)
+    if len(listRes) != 0:
+        print(f"Các số Q-Prime nhỏ hơn hoặc bằng {n} là: ", end="")
+        print(listRes)
+    else:
+        print("Không tìm thây số Q-Prime thoả yêu cầu!!!")
+    print()
+
+
+if __name__ == "__main__":
+    main()
